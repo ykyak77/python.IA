27f8c2f05edcbb000c26e669041a3cf9eb043fcb
Kayky Henrique <128718087+ykyak77@users.noreply.github.com>
2024-10-26 15:43:28 -0300

    Add files via upload

---
+++ python-IA/main.py
@@ -0,0 +1,26 @@
+from lib2to3.fixes.fix_input import context
+
+from flask import Flask, render_template, request
+from dotenv import load_dotenv
+from google import generativeai as genai
+import os
+
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/search')
+def search():
+    model = genai.GenerativeModel('gemini-1.0-pro-latest')
+    genai.configure(api_key=os.getenv('GEMINI_API_KEY'))
+    context = 'Responda como se fosse uma pessoa super inteligente'
+    prompt = request.args.get('prompt')
+    input_ia = f'{context}: {prompt}'
+    output = model.generate_content(input_ia)
+    return {'message': output.text}
+
+app.run(debug=True)
---
+++ python-IA/static/js/index.js
@@ -0,0 +1,47 @@
+async function fetchBotResponse(messageText) {
+    try {
+        const response = await fetch(`${window.location.href}search?prompt=${messageText}`);
+        const data = await response.json();
+        return data.message;
+    } catch (error) {
+        console.error('Erro ao buscar dados:', error);
+        return 'Desculpe, não consegui buscar uma resposta agora.';
+    }
+}
+
+async function sendMessage() {
+    const userInput = document.getElementById('userInput');
+    const chatWindow = document.getElementById('chatWindow');
+    const messageText = userInput.value.trim();
+
+    if (messageText) {
+        const userMessage = document.createElement('div');
+        userMessage.className = 'message user';
+        userMessage.innerHTML = `<span>${messageText}</span>`;
+        chatWindow.appendChild(userMessage);
+
+        userInput.value = '';
+
+        const loadingMessage = document.createElement('div');
+        loadingMessage.className = 'message bot loading';
+        loadingMessage.innerHTML = `<span>Carregando...</span>`;
+        chatWindow.appendChild(loadingMessage);
+        chatWindow.scrollTop = chatWindow.scrollHeight;
+
+        const botResponse = await fetchBotResponse(messageText);
+
+        chatWindow.removeChild(loadingMessage);
+        const botMessage = document.createElement('div');
+        botMessage.className = 'message bot';
+        botMessage.innerHTML = `<span>${botResponse}</span>`;
+        chatWindow.appendChild(botMessage);
+
+        chatWindow.scrollTop = chatWindow.scrollHeight;
+    }
+}
+
+function checkEnter(event) {
+    if (event.key === 'Enter') {
+        sendMessage();
+    }
+}
