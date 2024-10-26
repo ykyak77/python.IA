7a477dce89a4ad10206aeb7f0369c0c3e19ad526
Kayky Henrique <128718087+ykyak77@users.noreply.github.com>
2024-10-26 15:59:53 -0300

    Add files via upload

--- python-IA/main.py
+++ python-IA/main.py
@@ -23,4 +23,5 @@ def search():
     output = model.generate_content(input_ia)
     return {'message': output.text}
 
-app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=os.getenv('FLASH_DEBUG'))
